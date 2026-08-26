26623a6b6817ffcd2fda34e7c38db9a44ddcbc0c
kuligambino01 <czaro923@gmail.com>
2026-08-26 10:54:56 +0200

    feat: register worker, task, task type and position models in admin

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from task.models import TaskType, Task, Position, Worker
+
 
 # Register your models here.
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "is_completed", "priority", "task_type",)
+    search_fields = ("priority",)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = ("position",) + UserAdmin.list_display
+    fieldsets = UserAdmin.fieldsets
+    list_filter = ("position__name",)
