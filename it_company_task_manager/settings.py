50d15daa139e347d12021a8cb112a4cb66b7c190
kuligambino01 <czaro923@gmail.com>
2026-08-28 16:42:45 +0200

    fix: correct project settings for bootstrap5

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -35,9 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms'
-    'crispy_bootstrap5'
-    'task'
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'task',
 ]
 
 MIDDLEWARE = [
