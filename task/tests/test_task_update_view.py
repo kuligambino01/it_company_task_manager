ff3dc1651dcfba91c65969631dff90c4c0224266
kuligambino01 <czaro923@gmail.com>
2026-09-07 12:10:07 +0200

    style: format task update test

--- task/tests/test_task_update_view.py
+++ task/tests/test_task_update_view.py
@@ -107,7 +107,7 @@ class TaskUpdateViewTests(BaseTaskTestCase):
     def test_update_allows_unchanged_expired_deadline(self):
         self.client.force_login(self.user)
 
-        expired_deadline = (timezone.now() - timedelta(days=1))
+        expired_deadline = timezone.now() - timedelta(days=1)
 
         self.task.deadline = expired_deadline
         self.task.assignees.add(self.user)
