607bf3da0f5ac36b29110469ee4e27b538f0d682
kuligambino01 <czaro923@gmail.com>
2026-09-13 15:07:27 +0200

    fix: configure STATIC_ROOT for deployment

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -160,6 +160,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
